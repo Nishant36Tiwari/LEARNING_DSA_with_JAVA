ed1a4db9601d0f33354f69c922183be34d852b9b
Nishant Tiwari <NISHANT36TIWARI@GMAIL.COM>
2024-05-01 14:11:28 +0530

    A boiler plate for JAVA

---
+++ Boiler_plate.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Boiler_plate {
+
+    // function for ARRAY
+    public static void bubbleSort(int arr[]){
+        //CODES
+    }
+
+    //MAIN
+    public static void main(String args[]){
+        // ques:- Arr[]={1,3,3}
+    }
+}
