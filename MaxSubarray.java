5c43d1405cd4ae2d345aa6b68978e40f9ca42c49
Nishant Tiwari <NISHANT36TIWARI@GMAIL.COM>
2024-05-03 23:59:09 +0530

    max

---
+++ MaxSubarray.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+                int maxSum = nums[0];
+        int currentSum = nums[0];
+        
+        for (int i = 1; i < nums.length; i++) {
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+            maxSum = Math.max(maxSum, currentSum);
+        }
+        
+        return maxSum;
+
+    }
+}
+public static void main(String[] args) {
+    MaxSubarraySum solution = new MaxSubarraySum();
+    int[] nums = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+    System.out.println("Maximum subarray sum: " + solution.maxSubArray(nums));
+}
