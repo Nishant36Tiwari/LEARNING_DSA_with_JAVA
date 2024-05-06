9d812e6ae046c29b749b6c8fde2578e5bf465c97
Nishant Tiwari <NISHANT36TIWARI@GMAIL.COM>
2024-05-06 23:28:06 +0530

    counting sort Retested

--- AssgnmentAllSorting.java
+++ AssgnmentAllSorting.java
@@ -61,6 +61,7 @@ public class AssgnmentAllSorting {
         for (int i=0 ; i<n ; i++){
             count[arr[i]]++;
         }
+        System.out.println(count.length);
         //sorting
         int j=0;
         for (int i=0 ; i<count.length ; i++){
@@ -84,7 +85,8 @@ public class AssgnmentAllSorting {
     public static void main(String args[]){
         //int arr[]= {5,7,6,1,2,8,3};
         //int arr[]= {3,6,2,1,8,7,4,5,3,1};
-        int arr[]= {1,1,1,25,2,3,45,21,2,5,5,3,5,5,5,22,5};
+        //int arr[]= {1,1,1,25,2,3,45,21,2,5,5,3,5,5,5,22,5};
+        int arr[]= {1,255425};
         bubbleSort(arr);
         System.out.println();
         System.out.println("sorting using bubble Sorting");
